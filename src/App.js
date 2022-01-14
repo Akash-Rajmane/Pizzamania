caafa55a8791a68a8d9a62eb1a8038cf3d3731a3
Akash Rajmane <90460418+Akash-Rajmane@users.noreply.github.com>
2022-01-14 15:22:21 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
         <Navbar/>
         <Home/>
         <Routes>
-          <Route path="/" exact element={<Home/>}/>
+          <Route path="/" element={<Home/>}/>
           <Route path="/menu" exact element={<Menu/>} />
           <Route path="/about" exact element={<About/>} />
           <Route path="/contact" exact element={<Contact/>} />
